9f077fc29a21744c2520a46632ceeeee0c5836f4
Bethel Irumudomon <bethel.iru@gmail.com>
2019-04-22 18:07:21 +0100

    CMM007 CourseWork
    RGU ONLINE UNION SHOP

--- customer/functions/functions.php
+++ customer/functions/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = mysqli_connect("localhost","root","","ecom_store");
+$db = mysqli_connect("CSDM-WEBDEV","1715122","1715122","db1715122_rgu_store");
 
 /// IP address code starts /////
 function getRealUserIp(){
--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,5 @@
 <?php
 
-$con = mysqli_connect("localhost","root","","ecom_store");
+$con = mysqli_connect("CSDM-WEBDEV ","1715122","1715122","db1715122_rgu_store");
 
 ?>
